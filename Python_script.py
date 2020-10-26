5837e778008cda70bdf02919f6f33f57c2f31861
theskyspace <akashrock411@gmail.com>
2020-10-26 17:53:45 +0530

    edit in code

--- Python_script.py
+++ Python_script.py
@@ -9,8 +9,16 @@ files=os.listdir(path)
 
 for i in range(10):
 	d=random.choice(files)
+	print(d)
 	os.startfile(d)
 	cont = int(input("Do you want to continue with this?(1/0)"))
-	if cont = 1:
+	if cont == 1:
 		time.sleep(15)
 		winsound.Beep(300, 10000)
+		print(write)
+		time.sleep(215)
+		winsound.Beep(400, 10000)
+		time.sleep(10)
+		winsound.Beep(500, 10000)
+	else:
+		continue
